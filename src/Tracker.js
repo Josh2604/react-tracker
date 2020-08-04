8f74893d74665f513d3439292ce8cf331fa809ee
Josh Andres <josh.an.bas@gmail.com>
2020-08-04 12:31:20 -0500

    Unused vars

--- src/Tracker.js
+++ src/Tracker.js
@@ -4,20 +4,19 @@ export const AppContext = React.createContext();
 
 export function AppTracker({
   app_data,
-  children,
-  inject
+  children
 }){
   return(
-    <AppContext.Provider value={{app_data, inject}}>
+    <AppContext.Provider value={{ app_data }}>
       {children}
     </AppContext.Provider>
   );
 }
 
-export default function withAppTracker(Component, {app_data, inject}){
+export default function withAppTracker(Component, { app_data }){
   return function withAppTracker(props){
     return (
-      <AppContext.Provider value={{ app_data, inject}}>
+      <AppContext.Provider value={{ app_data }}>
         <Component {...props}/>
       </AppContext.Provider>
     );
